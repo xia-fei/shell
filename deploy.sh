fca63e3fc390473c70f5e01b9f35bf56d64dbc36
xia-fei <xiafei.dev@gmail.com>
2019-08-09 22:31:56 +0800

    shell

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,6 @@ fi
 
 
 image-server(){
-#所有前端项目
 cd /tmp
 git clone https://github.com/xia-fei/file-server.git
 cd file-server
@@ -22,4 +21,13 @@ mvn package -Dmaven.test.skip=true
 nohup java -Daccess.key.secret="QGWGUdG8wxbNTz1QCQ05SbNifXXZ7n" -Daccess.key.id="LTAIX3Mkmhdk2GeB" -Dserver.port=8002 -jar target/file-0.0.1-SNAPSHOT.jar >file-server.log 2>&1 &
 }
 
+
+sql(){
+cd /tmp && \
+git clone https://github.com/xia-fei/toolbox-web.git &&\
+cd toolbox-web/sql-resolve && \
+npm run build && \
+rm -rf /usr/share/nginx/sql-resolve && \
+cp -rf dist /usr/share/nginx/sql-resolve
+}
 www-blog
