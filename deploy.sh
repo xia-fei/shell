e08af269f10e7c75335accb59ac319e8948b729c
夏飞 <xiafei.dev@gmail.com>
2019-10-08 20:33:17 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,10 @@ basePath=/usr/share/nginx
 
 www-blog(){
 repoName='xia-fei.github.io';
-if [ ! -d $basePath/$repoName ]; then
+rm -f $basePath/$repoName 
 git clone https://github.com/xia-fei/${repoName}.git $basePath/$repoName
-else
-cd $basePath/$repoName
-echo $(pwd)
-git pull 
-fi
-}
 
+}
 
 image-server(){
 cd /tmp
