a13b6fe53cc381dec4e5cfa9f0f28398aa2ea814
xia-fei <xiafei.dev@gmail.com>
2019-12-09 17:38:34 +0800

    commit

--- start.sh
+++ start.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+
+echo "请将该脚本放在项目的mvn package执行的目录执行";
+echo "它将执行mvn package";
+echo "匹配target目录的jar文件名称,kill掉进程";
+
+SERVER_PORT=8080;
+if [[ ! -z $1 ]]; then
+    SERVER_PORT=$1;
+fi
+
+
 mvn clean package -Dmaven.test.skip=true
 
-JVM_OPS="-Dserver.port=8001 -Xmx64m"
+JVM_OPS="-Dserver.port=$SERVER_PORT -Xmx64m"
 
 APP_NAME=$(ls target|grep .*jar$ );
-PID=$(ps -ef |grep $APP_NAME |grep -v grep |awk '{print $2}');
+PID=$(ps -ef |grep ${APP_NAME} |grep -v grep |awk '{print $2}');
 
 echo " ----------------------------------------"
-echo "|  APP_NAME:"$APP_NAME"  PID:"$PID
+echo "|  APP_NAME:"$APP_NAME"  PID:"${PID}  JVM_OPS=${JVM_OPS}"
 echo " ----------------------------------------"
 
 kill  $PID
---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[  -z $1 ]]; then
+    echo "没参数"
+else
+    echo "有参数"$1;
+fi
+
+exit;
