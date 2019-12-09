6f60cd02ceb8b28361f3e2c7625d3fea84d3040d
xia-fei <xiafei.dev@gmail.com>
2019-12-09 17:21:15 +0800

    commit

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,9 @@ basePath=/usr/share/nginx
 
 www-blog(){
 repoName='xia-fei.github.io';
-if [ ! -d $basePath/$repoName ]; then
+rm -f $basePath/$repoName
 git clone https://github.com/xia-fei/${repoName}.git $basePath/$repoName
-else
-cd $basePath/$repoName
-echo $(pwd)
-git pull 
-fi
+
 }
 
 
---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mvn clean package -Dmaven.test.skip=true
+
+JVM_OPS="-Dserver.port=8001 -Xmx64m"
+
+APP_NAME=$(ls target|grep .*jar$ );
+PID=$(ps -ef |grep $APP_NAME |grep -v grep |awk '{print $2}');
+
+echo " ----------------------------------------"
+echo "|  APP_NAME:"$APP_NAME"  PID:"$PID
+echo " ----------------------------------------"
+
+kill  $PID
+
+sleep 2s;
+
+nohup  java $JVM_OPS -jar  target/$APP_NAME &
+
+
